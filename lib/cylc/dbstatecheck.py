b2d9980d1a544fc3d7978f4b16139a50f093a038
Thomas Coleman <thomas.coleman@bom.gov.au>
2019-12-27 14:37:22 +1100

    Add latest time check method for an event from database

--- lib/cylc/dbstatecheck.py
+++ lib/cylc/dbstatecheck.py
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from datetime import datetime
 import errno
 import json
 import os
@@ -77,9 +78,6 @@ class CylcSuiteDBChecker(object):
     def suite_state_query(
             self, task, cycle, status=None, message=None, mask=None):
         """run a query on the suite database"""
-        stmt_args = []
-        stmt_wheres = []
-
         if mask is None:
             mask = "name, cycle, status"
 
@@ -90,19 +88,7 @@ class CylcSuiteDBChecker(object):
             target_table = CylcSuiteDAO.TABLE_TASK_STATES
 
         stmt = "select {0} from {1}".format(mask, target_table)
-        if task is not None:
-            stmt_wheres.append("name==?")
-            stmt_args.append(task)
-        if cycle is not None:
-            stmt_wheres.append("cycle==?")
-            stmt_args.append(cycle)
-
-        if status:
-            stmt_frags = []
-            for state in self.state_lookup(status):
-                stmt_args.append(state)
-                stmt_frags.append("status==?")
-            stmt_wheres.append("(" + (" OR ").join(stmt_frags) + ")")
+        stmt_args, stmt_wheres = self._setup_task_query(task, cycle, status)
         if stmt_wheres:
             stmt += " where " + (" AND ").join(stmt_wheres)
 
@@ -128,6 +114,74 @@ class CylcSuiteDBChecker(object):
                     if message == value:
                         return True
             return False
+
+    def get_last_time_state_met(self, task, cycle, status=None, message=None):
+        """used to get the most recent time that a status or message has
+        been met. Returns a UTC datetime object.
+
+        Args:
+            task (string): The task to search for
+            cycle (string): The cycle point of the task
+            status (string): The status to search for
+            message (string): The message to search for
+
+        Returns:
+            datetime: date of the last found matching status or message event
+        """
+        if not self.task_state_met(task, cycle, status, message):
+            return None
+
+        stmt_args, stmt_wheres = self._setup_task_query(
+                task, cycle, status, "event")
+
+        # Append the message string if a message is provided
+        if message is not None:
+            stmt_args.append(message)
+            stmt_wheres.append("message==?")
+
+        # Construct the select statement, converting the date text entry
+        # into a UTC date, and then taking only the single latest row
+        target_table = CylcSuiteDAO.TABLE_TASK_EVENTS
+        stmt = "SELECT DATETIME(time) FROM " + target_table
+        if stmt_wheres:
+            stmt += " WHERE " + (" AND ").join(stmt_wheres)
+        stmt += " ORDER BY time DESC LIMIT 1"
+
+        result = self.conn.execute(stmt, stmt_args).fetchone()[0]
+        return datetime.strptime(result, "%Y-%m-%d %H:%M:%S")
+
+    def _setup_task_query(self, task=None, cycle=None, status=None,
+                          status_label="status"):
+        """Helper method to construct the where clause for a SQL request.
+
+        Args:
+            task (string): The task to search for
+            cycle (string): The cycle point of the task
+            status (string): The status expected
+            status_label (string): The column header the task status will be in
+
+        Returns:
+            list 1: Cursor values
+            list 2: SQL WHERE statement components
+
+        """
+        stmt_args = []
+        stmt_wheres = []
+        if task is not None:
+            stmt_wheres.append("name==?")
+            stmt_args.append(task)
+        if cycle is not None:
+            stmt_wheres.append("cycle==?")
+            stmt_args.append(cycle)
+
+        if status:
+            status_label += "==?"
+            stmt_frags = []
+            for state in self.state_lookup(status):
+                stmt_args.append(state)
+                stmt_frags.append(status_label)
+            stmt_wheres.append("(" + (" OR ").join(stmt_frags) + ")")
+        return stmt_args, stmt_wheres
 
     @staticmethod
     def validate_mask(mask):
